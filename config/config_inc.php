1a1589ac9a7d29c4bb174c95bf921237c6a3033f
Brenner Cunha <brennersc@gmail.com>
2020-06-29 11:59:21 -0300

    Mantis Fumec

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantis';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'America/Sao_Paulo';
+
+$g_crypto_master_salt     = '3auzMD0zLHXqAlnkWb5biGgF1JO21zoa2ybx1tcLfSg=';
